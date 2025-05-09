60324f149621cd66530ece775866a4ad34270b99
BXILLEE <bail.leescot@gmail.com>
2025-05-09 18:57:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 function addToCart(productName) {
   alert(productName + " added to cart!");
 }
-
-document.getElementById("themeToggle").addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  const icon = document.getElementById("themeToggle");
-  icon.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
-});
